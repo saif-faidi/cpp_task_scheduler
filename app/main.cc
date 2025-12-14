4eb7b964a66a6fbc0eb2dc54cec5b110ece0645a
saif-faidi <saiffaidi28@gmail.com>
2025-12-14 20:26:39 +0100

    lazy initialization

--- app/main.cc
+++ app/main.cc
@@ -14,51 +14,72 @@
 // #include"thread_pool.h"
 // #include"para_execution.h"
 
+#include <algorithm>
+#include <execution>
 #include <future>
 #include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <shared_mutex>
 #include <stdexcept>
 #include <type_traits>
-#include<random>
-#include<numeric>
 
 
+using namespace std::literals;
+std::mutex mtx;
+
+struct Test
+{
+
+    void execute()
+    {
+        spdlog::info("execute");
+    }
+};
+
+Test *ptest = nullptr;
+
+
+void process()
+{
+    if (!ptest) // if ptest !=nullptr -> exceute directly there is no need to lock and unlock 
+    {   
+        std::lock_guard<std::mutex> lock(mtx);
+        {   // maybe another thread already locked and unlocked the mutex , we need to retest if ptest is still nullprtr
+            if (!ptest)
+            {
+                ptest = new Test(); // memory allocation , 
+                    //creation of test pointer
+                    // allocation to ptest ( 3 steps in one)
+            }
+        }
+
+    }
+    ptest->execute();
+}
 
 int main()
 {
 
+    std::vector<std::jthread> jthreads;
+
+    for (size_t i = 0; i < 5; ++i)
+    {
+        jthreads.push_back(std::jthread(process));
+    }
+
+    for (auto &th : jthreads)
+    {
+        th.join();
+    }
+
+    delete ptest;
+
+    spdlog::info("Program Finished...\n");
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //power_main_example();
